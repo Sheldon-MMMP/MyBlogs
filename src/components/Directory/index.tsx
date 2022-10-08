fc2d3014908e3764ca4e128ebc04cdccf7f86a81
Alex <2512989241@qq.com>
2022-10-08 09:30:28 +0800

    创建文章结构

---
+++ src/components/Directory/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Directory = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+
+export default Directory
--- src/pages/blog/[species].tsx
+++ src/pages/blog/[species].tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import Head from "next/head"
 import Header from '../../components/Header'
+import Directory from '../../components/Directory'
+import Content from '../../components/Content'
 
 const Blog: React.FC = () => {
 
@@ -14,6 +16,10 @@ const Blog: React.FC = () => {
         <title>{species}</title>
       </Head>
       <Header title={species as string}></Header>
+      <div>
+        <Directory></Directory>
+        <Content></Content>
+      </div>
     </div>
   )
 }
