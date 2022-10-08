77e9a88770a0e8f3905741c3ae7d6d6f96a2b412
Alex <2512989241@qq.com>
2022-10-08 16:38:39 +0800

    添加一个logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import styles from './header.module.scss'
+import Image from 'next/image'
+import Link from "next/link"
 
 interface classificationDefine {
   id: number,
@@ -17,7 +19,12 @@ const Header = ({ title }: { title: string }) => {
   const [optionIndex, setOptionIndex] = useState(1)
   return (
     <header className={`flex justify-between px-30 items-center ${styles.shadow} h-15`}>
-      <h1 className="text-3xl font-800">{title}</h1>
+      <div className='flex w-40 justify-between'>
+        <Link href="/">
+          <Image src="/icon.png" width={34} height={34}></Image>
+        </Link>
+        <h1 className="text-3xl font-800">{title}</h1>
+      </div>
       <ul className='flex w-80 justify-between' >
         {
           classification.map((item, index) => <li key={index} className={`${item.id === optionIndex ? "text-dark-900" : "text-primarily"} cursor-pointer`} onClick={() => setOptionIndex(item.id)} > {item.name}</li>)
