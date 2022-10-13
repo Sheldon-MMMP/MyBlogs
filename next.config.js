ce408531f5283915ea3057a3e4dd5e55951dd538
Alex <2512989241@qq.com>
2022-10-13 15:23:07 +0800

    更改配置

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 const path = require('path');
 
-module.exports = (phase, { defaultConfig }) => {
-  const nextConfig = {
-    webpack(config) {
-      config.plugins.push(new WindiCSSWebpackPlugin())
-      return config
-    },
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-  }
-  return nextConfig
+module.exports = {
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  "plugins": ["styled-jsx/babel"]
 }
