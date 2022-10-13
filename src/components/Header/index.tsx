4b199c018c0877148b9185add60d93f651832d76
Alex <2512989241@qq.com>
2022-10-13 17:06:44 +0800

    修改错误一处，Link不能包子组件

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styles from './header.module.scss'
+import styles from "./header.module.scss"
 import Image from 'next/image'
 import Link from "next/link"
 
@@ -15,22 +15,29 @@ const classification: classificationDefine[] = [
 ]
 
 
-const Header = ({ title }: { title: string }) => {
-  const [optionIndex, setOptionIndex] = useState(1)
+const Header = ({ title, getMsg }: { title: string, getMsg: Function }) => {
+  const [optionIndex, setOptionIndex] = useState(1);
+
+  const onOptionIndex = ({ name, id }: { name: string, id: number }) => {
+    setOptionIndex(id)
+    getMsg(name)
+  }
   return (
-    <header className={`flex justify-between px-30 items-center ${styles.shadow} h-15`}>
+    <header className={`flex justify-between px-30 items-center ${styles.shadow} h-15 min-w-300 z-10`}>
       <div className='flex w-40 justify-between'>
         <Link href="/">
-          <Image src="/icon.png" width={34} height={34}></Image>
+          <div>
+            <Image src="/icon.png" width={34} height={34}></Image>
+          </div>
         </Link>
         <h1 className="text-3xl font-800">{title}</h1>
       </div>
       <ul className='flex w-80 justify-between' >
         {
-          classification.map((item, index) => <li key={index} className={`${item.id === optionIndex ? "text-dark-900" : "text-primarily"} cursor-pointer`} onClick={() => setOptionIndex(item.id)} > {item.name}</li>)
+          classification.map((item, index) => <li key={index} className={`${item.id === optionIndex ? "text-dark-900" : "text-primarily"} cursor-pointer`} onClick={() => onOptionIndex(item)} > {item.name}</li>)
         }
       </ul>
-    </header >
+    </header>
   )
 }
 
